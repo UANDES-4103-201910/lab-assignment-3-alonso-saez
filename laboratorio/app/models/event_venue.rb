8f3958b77fe888a00858b2d05dcba47457050d10
Antonia Saez <asaez1@miuandes.cl>
2019-03-28 13:57:13 -0300

    metodos

--- laboratorio/app/models/event_venue.rb
+++ laboratorio/app/models/event_venue.rb
@@ -1,3 +1,7 @@
 class EventVenue < ApplicationRecord
 	has_many :events
+	def last_attendace
+		a = self.events.find(:startdate).order(startdate: :desc).tickets.count.limit(1)
+		return self.events.find(:startdate).tickets.count.order(startdate: :desc).limit(1)
+	end
 end
--- laboratorio/app/models/user.rb
+++ laboratorio/app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
 	has_many :tickets
-	tickets = User.tickets
-	def most_expensive_ticket_bought (tickets)
-		prices = Ticket.User.find()
-
-		return ticket.find (:user_id).find (:price).order (price: :desc).limit(1)
+	def most_expensive_ticket_bought
+		return self.tickets.ticket_type.find(:price).order(price: :desc).limit(1)
 	end
-	def most_expensive_ticket_between ()
+	def most_expensive_ticket_between (Date a, Date b)
+		ticket.find(:ticket_type).find(ticket_type: :price).find(:event).find(event: :startdate).where(startdate < b, startdate > a).order(price: :desc).limit(1)
 		return ticket.find
 	end
 end
